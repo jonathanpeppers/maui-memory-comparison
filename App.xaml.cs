7a616400d329fb75a2d7148041987524ecbebc50
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-09-28 09:08:26 -0500

    Set window size smaller on desktop

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MemoryComparison;
+﻿
+namespace MemoryComparison;
 
 public partial class App : Application
 {
@@ -8,4 +9,14 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+#if WINDOWS || MACCATALYST
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		window.Width = 300;
+		window.Height = 550;
+		return window;
+	}
+#endif
 }
