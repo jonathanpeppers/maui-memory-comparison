6a06ca996b9cafca471234959d94b61c6b6b299e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-09-28 13:57:04 -0500

    Fixed app titles

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,5 +5,11 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+
+#if NET8_0
+		Title = ".NET 8 Memory";
+#elif NET7_0
+		Title = ".NET 7 Memory";
+#endif
 	}
 }
